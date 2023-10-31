f0daa4c4bff28de37702e89f36305ff5dd3ea247
Ganesh Jagdale <jagdaleganesh9545@gmail.com>
2023-10-31 12:14:58 +0530

    update packages versions

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     },
     install_requires=[
         "ansicolors~=1.1.8",
-        "boto3~=11.24.65",
+        "boto3~=1.24.65",
         "botocore~=1.27.65",
         "jmespath~=1.0.1",
         "pyyaml~=4.2b4",
